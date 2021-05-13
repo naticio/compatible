aa740253e60d5d8568e6d886e9bac7b75def2ec3
Nat-Serrano <naticio@gmail.com>
2021-05-13 13:54:53 -0500

    Initial Commit

---
+++ Compatible/CompatibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompatibleApp.swift
+//  Compatible
+//
+//  Created by Nat-Serrano on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CompatibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
